06cc6cae60f4dd83da606e8f4c11923a96005fb0
Olawale Mayor <olawalemayor90@gmail.com>
2022-05-30 07:54:50 +0100

    corrected mistake in port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/commodule/index.html"));
 });
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
